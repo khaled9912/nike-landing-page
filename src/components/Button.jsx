e7651f9e38808a7442f8f3a0bfcf5b1e872f1a50
khaled9912 <khaledmourad23345@gmail.com>
2024-06-28 00:25:27 +0300

    finalizing superquality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,13 +5,14 @@ const Button = ({ label, iconURL }) => {
         rounded-full text-white border-coral-red">
            {label}
 
-           <img 
-            src={iconURL}
-            alt="arrow right icon"
-            className="ml-2 rounded-full w-5 h-5"
-           />
+          {iconURL && <img 
+               src={iconURL}
+               alt="arrow right icon" 
+               className="ml-2 rounded-full w-5 h-5"
+              />
+          } 
         </button>
-)
+         )
 }
 
-export default Button
+export default Button;
